7ca66db3fd9b41f66c84d456eec3b5abd9ad7256
Adam Storr <astorr@gmail.com>
2021-08-08 20:00:12 +0100

    Initial project setup. Add data model and db context with db initalization setup.

---
+++ src/Spotter/Data/SpotterContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Spotter.Models;
+
+namespace Spotter.Data
+{
+    public class SpotterContext : DbContext
+    {
+        public SpotterContext(DbContextOptions<SpotterContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Plane> Plane { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Plane>().ToTable("Plane");
+        }
+
+#if DEBUG
+        /// <summary>
+        /// Initialize a small data set for testing purposes. This is only use in debug mode only.
+        /// </summary>
+        /// <param name="context"></param>
+        public static void Initialize(SpotterContext context)
+        {
+            if (context.Plane.Any())
+            {
+                return;
+            }
+
+            context.Plane.Add(new Plane
+            {
+                Make = "Boeing",
+                Model = "777-300ER",
+                Registration = "G-RNAC",
+                Location = "London Gatwick",
+                SeenDateTime = DateTime.Now.AddDays(-2)
+            });
+            context.SaveChanges();
+        }
+#endif
+    }
+}
---
+++ src/Spotter/Models/Plane.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Spotter.Models
+{
+    public class Plane
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        //[MaxLength(128, ErrorMessage = "Make can only be 128 characters long.")]
+        public string Make { get; set; }
+
+        //[MaxLength(128, ErrorMessage = "Model can only be 128 characters long.")]
+        public string Model { get; set; }
+
+        //[RegularExpression("[A-Z]{0,2}(-)?[A-Z]{1,5}")]
+        public string Registration { get; set; }
+
+        public string Location { get; set; }
+
+        public DateTime SeenDateTime { get; set; }
+    }
+}
